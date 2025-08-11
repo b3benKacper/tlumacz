fcb4977a42689af98a0f312cd9c986587c0a0c13
b3benKacper <b3ben.kacper@gmail.com>
2025-08-11 08:52:30 +0200

    zmiana modeli
    zmiana modeli
    zmiana prompta na angielski

--- tlumacz.py
+++ tlumacz.py
@@ -6,12 +6,9 @@ import pyperclip
 address="http://localhost:11434/api/generate"
 text = pyperclip.paste()
 languages = {
-    ("pl", "de"): "qwen2.5:latest",
-    ("pl", "en"): "qwen2.5:latest",
-    ("de", "pl"): "qwen2.5:latest",
-    ("de", "en"): "qwen2.5:latest",
-    ("en", "de"): "qwen2.5:latest",
-    ("en", "pl"): "qwen2.5:latest",
+    "de": "jobautomation/openeurollm-german:latest",
+    "en": "llama3:latest",
+    "pl": "jobautomation/openeurollm-polish:latest"
 }
 
 if len(sys.argv) == 3 :
@@ -21,7 +18,7 @@ else:
     print("podaj 2 argumenty")
     sys.exit()
     
-languages_keys = (sys.argv[1], sys.argv[2])
+languages_keys = (sys.argv[2])
 
 if languages_keys not in languages:
     print("Nieobsługiwana para języków.")
@@ -32,9 +29,10 @@ if languages_keys in languages:
 
 playload = {
     "model":model,
-    "prompt":f''' "Jesteś profesjonalnym tłumaczem. Podam tekst, język źródłowy i docelowy. Tłumacz dokładnie, zachowując sens, styl i kontekst oryginału. Nie dodawaj wyjaśnień ani komentarzy — podaj wyłącznie przetłumaczony tekst. TEKST: '{text}', JĘZYK DOCELOWY: '{to_language}', JĘZYK ŹRÓDŁOWY: '{from_language}'" ''',
+    "prompt":f''' "You are a professional translator. I will provide the text, source language, and target language. Translate accurately, preserving the meaning, style, and context of the original. Do not add explanations or comments—provide only the translated text. TEXT: '{text}', TARGET LANGUAGE: '{to_language}', SOURCE LANGUAGE: '{from_language}'" ''',
     "stream":False
 }
+
 headers = {
     "Content-Type": "application/json"
 }
@@ -47,3 +45,6 @@ try:
 except json.JSONDecodeError:
     print("Błąd")
     print(response.text)
+    
+    
+#python tlumacz.py {j.źródłowy} {j.docelowy} 
